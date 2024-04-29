8ab09793fe7261f0eb01cf8931b3666e27adeef6
lukazashovski <luka.zasovski@gmail.com>
2024-04-29 21:00:43 +0200

    finished project blyaaat

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,142 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [menuActive, setMenuActive] = useState(false);
+  const [playClicked, setPlayClicked] = useState(false);
+  const [mainVisible, setMainVisible] = useState(false);
+  const [itemCount, setItemCount] = useState();
+  const [items, setItems] = useState([]);
+  const [score, setScore] = useState(0);
+  const [clickedItemIds, setClickedItemIds] = useState(new Set());
+  const [winScreenVisible, setWinScreenVisible] = useState(false);
+
+  const handlePlayClick = () => {
+    setPlayClicked(true);
+    setMenuActive(true);
+  };
+
+  const handleMenuClick = () => {
+    setPlayClicked(false);
+    setMenuActive(false);
+    setScore(0);
+  };
+
+  const handleDifficultyClick = (count) => {
+    setPlayClicked(true);
+    setMenuActive(false);
+    setMainVisible(true);
+    setItemCount(count);
+  };
+
+  const handleGameClick = () => {
+    setMainVisible(false);
+    setMenuActive(true);
+    setScore(0);
+  };
+
+  const getRandomItems = (list, count) => {
+    const shuffledItems = [...list].sort(() => Math.random() - 0.5);
+    return shuffledItems.slice(0, count);
+  };
+
+  useEffect(() => {
+    fetch("https://bymykel.github.io/CSGO-API/api/en/skins.json")
+      .then((response) => response.json())
+      .then((json) => {
+        const shuffledItems = getRandomItems(json, itemCount);
+        setItems(shuffledItems.map((item, index) => ({ ...item, id: index })));
+      });
+  }, [mainVisible]);
+
+  useEffect(() => {
+    if (score === itemCount && score !== 0) {
+      setMainVisible(false);
+      setWinScreenVisible(true);
+      setScore(0);
+    }
+  }, [score, itemCount]);
+
+  useEffect(() => {
+    if (score === 0) {
+      // Reset clicked items when score is 0
+      setClickedItemIds(new Set());
+    }
+  }, [score]);
+
+  const onItemClick = (itemId) => {
+    if (clickedItemIds.has(itemId)) {
+      setScore(0);
+      setClickedItemIds(new Set());
+    } else {
+      setScore(score + 1);
+      setClickedItemIds(new Set(clickedItemIds).add(itemId));
+    }
+
+    const shuffledItems = [...items].sort(() => Math.random() - 0.5);
+    setItems(shuffledItems);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+      <div className={`main-menu ${playClicked ? "hidden" : ""}`}>
+        <img
+          src="cs2-skin-memory.png"
+          alt="an awesome image for the logo of the game"
+        />
+        <a className="play-button" onClick={handlePlayClick}>
+          play GAME cyka
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className={`difficulty-menu ${menuActive ? "active" : ""}`}>
+        <div className="menu-actions">
+          <p>choose difficulty blyaaat</p>
+          <a onClick={handleMenuClick}>&rarr;</a>
+        </div>
+        <div className="difficulty-buttons">
+          <a onClick={() => handleDifficultyClick(6)}>noob</a>
+          <a onClick={() => handleDifficultyClick(12)}>rizzler</a>
+          <a onClick={() => handleDifficultyClick(20)}>hakcer!</a>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className={`main ${mainVisible ? "visible" : ""}`}>
+        <div className="overview">
+          <p>Score: {score}</p>
+          <a onClick={handleGameClick}>&rarr;</a>
+        </div>
+
+        <div className={"game"}>
+          {items.map((item) => (
+            <a key={item.id} onClick={() => onItemClick(item.id)}>
+              <img src={item.image} alt={item.name} />
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className={`win-screen ${winScreenVisible ? "visible" : ""}`}>
+        <p>YOU WIN BLYAAAAT!!</p>
+        <div className="options">
+          <a
+            onClick={() => {
+              setWinScreenVisible(false);
+              setMenuActive(true);
+            }}
+          >
+            go back na hui
+          </a>
+          <a
+            onClick={() => {
+              setWinScreenVisible(false);
+              setMainVisible(true);
+              setScore(0);
+            }}
+          >
+            restart 😎
+          </a>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
